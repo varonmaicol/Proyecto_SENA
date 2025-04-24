0c847df7f3ff748571ad4e42d60be71d6b127b05
MaicolVaron <varonmaicol313@gmail.com>
2025-04-24 13:41:01 -0500

    actividad

---
+++ repaso.py
@@ -0,0 +1,13 @@
+#creai ciclo 
+
+Ejercicio = int(input("ingrese el los grados de su cuerpo: "))
+
+if Ejercicio <= 80 :
+    print ("El cuerpo esta freso") 
+    
+else:
+    print ("Neceita ir de inmediato al hotipal")
+    print ("colapso del cuerpo en unos segundos")
+
+print("Fin de la labor")
+    
